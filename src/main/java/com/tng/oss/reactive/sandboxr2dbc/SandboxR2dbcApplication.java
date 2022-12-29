25a121d4781db16b69a6c9bb713398e39e8a9346
David Gao <dvdgjm@hotmail.com>
2022-12-29 23:17:30 +0800

    start.spring.io generated

---
+++ src/main/java/com/tng/oss/reactive/sandboxr2dbc/SandboxR2dbcApplication.java
@@ -0,0 +1,13 @@
+package com.tng.oss.reactive.sandboxr2dbc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SandboxR2dbcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SandboxR2dbcApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tng/oss/reactive/sandboxr2dbc/SandboxR2dbcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tng.oss.reactive.sandboxr2dbc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SandboxR2dbcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
